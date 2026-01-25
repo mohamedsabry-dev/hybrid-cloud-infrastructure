559a93ff4724fc3bf157c71cd1fae58c160962c3
mohamedsabry-dev <mohamedsabry.dev@gmail.com>
2026-01-25 13:08:24 +0200

    add comments

---
+++ terraform/aws/bootstrap/backend.tf
@@ -0,0 +1,4 @@
+# Setup wuthin the Issue INFRA-292  - 
+# 1. Configure S3 backend for Terraform state storage
+# 2. Configure DynamoDB table for state locking
+# Configure S3 backend for Terraform state storage & Push the current build state to the S3 backend
