d79701de5c0100ace5ccabea9e5cb1c09420fc5d
mohamedsabry-dev <mohamedsabry.dev@gmail.com>
2026-01-27 07:03:26 -0500

    edit on output runtf-bootstrap

--- terraform/aws/test/outputs.tf
+++ terraform/aws/test/outputs.tf
@@ -5,14 +5,29 @@ output "account_id" {
   value       = data.aws_caller_identity.current.account_id
 }
 
+output "assumed_role_arn" {
+  description = "ARN of the role assumed by GitHub Actions"
+  value       = data.aws_caller_identity.current.arn
+}
+
+output "s3_bucket_name" {
+  description = "Terraform state bucket name"
+  value       = data.aws_s3_bucket.terraform_state.bucket
+}
+
 output "s3_bucket_arn" {
   description = "Terraform state bucket ARN"
   value       = data.aws_s3_bucket.terraform_state.arn
 }
 
-output "s3_bucket_versioning" {
-  description = "S3 bucket versioning status"
-  value       = data.aws_s3_bucket.terraform_state.versioning[0].enabled
+output "s3_bucket_region" {
+  description = "S3 bucket region"
+  value       = data.aws_s3_bucket.terraform_state.region
+}
+
+output "dynamodb_table_name" {
+  description = "State lock table name"
+  value       = data.aws_dynamodb_table.state_lock.name
 }
 
 output "dynamodb_table_arn" {
