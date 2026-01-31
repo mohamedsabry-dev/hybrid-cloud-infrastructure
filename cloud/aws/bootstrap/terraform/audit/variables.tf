321e29debe4dea965404dfef29dafcf6fa496026
Sabry <mohamedsabry.dev@gmail.com>
2026-01-31 16:16:39 +0200

    update TF Code

--- cloud/aws/bootstrap/terraform/audit/variables.tf
+++ cloud/aws/bootstrap/terraform/audit/variables.tf
@@ -1,10 +1,10 @@
-variable audit_bucket {
+variable "audit_bucket" {
   type        = string
   default     = "hybrid-cloud-infrastructure-audit-logs"
   description = "The name of the existing S3 bucket for audit logs"
 }
 
-variable cloudtrail_name {
+variable "cloudtrail_name" {
   type        = string
   default     = "hybrid-main-trail"
   description = "The name of the CloudTrail trail"
