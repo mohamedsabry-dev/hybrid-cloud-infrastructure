50ee6749b6b5a059c88e48332a0739c90252e288
Sabry <mohamedsabry.dev@gmail.com>
2026-02-06 21:53:44 +0200

    update role and policy for readonly user

--- terraform/dev/aws/secrets/outputs.tf
+++ terraform/dev/aws/secrets/outputs.tf
@@ -7,3 +7,13 @@ output "secret_name" {
   value       = aws_secretsmanager_secret.proxmox_terraform.name
   description = "Name of the Proxmox secret"
 }
+
+output "plan_readonly_secret_arn" {
+  value       = aws_secretsmanager_secret.proxmox_plan_readonly.arn
+  description = "ARN of the Proxmox plan-readonly secret"
+}
+
+output "plan_readonly_secret_name" {
+  value       = aws_secretsmanager_secret.proxmox_plan_readonly.name
+  description = "Name of the Proxmox plan-readonly secret"
+}
