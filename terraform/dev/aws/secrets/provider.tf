aaa30b9ee0fb4d5b5e62f4b2989dd780ddbb6fc7
Sabry <mohamedsabry.dev@gmail.com>
2026-02-06 21:05:20 +0200

    update backend tfstate

--- terraform/dev/aws/secrets/provider.tf
+++ terraform/dev/aws/secrets/provider.tf
@@ -12,7 +12,7 @@ terraform {
 
   backend "s3" {
     bucket         = "hybrid-cloud-infrastructure-tf-state-eu"
-    key            = "dev/aws/iam/terraform.tfstate"
+    key            = "dev/aws/secrets/terraform.tfstate"
     region         = "eu-west-2"
     encrypt        = true
     dynamodb_table = "hybrid-cloud-infrastructure-tf-state-lock-eu"
--- terraform/prod/aws/secrets/provider.tf
+++ terraform/prod/aws/secrets/provider.tf
@@ -14,7 +14,7 @@ terraform {
 
   backend "s3" {
     bucket         = "hybrid-cloud-infrastructure-tf-state-eu"
-    key            = "prod/aws/iam/terraform.tfstate"
+    key            = "prod/aws/secrets/terraform.tfstate"
     region         = "eu-west-2"
     encrypt        = true
     dynamodb_table = "hybrid-cloud-infrastructure-tf-state-lock-eu"
