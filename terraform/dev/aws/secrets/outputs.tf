e75a2f5c6bf9fa4cb568707b49d17691ceeda3ff
Sabry <mohamedsabry.dev@gmail.com>
2026-02-06 18:21:13 +0200

    update & clean

--- terraform/dev/aws/secrets/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "proxmox_secret_arn" {
-  description = "ARN of the Proxmox API token secret"
-  value       = aws_secretsmanager_secret.proxmox_api_token.arn
-}
-
-output "proxmox_secret_name" {
-  description = "Name of the Proxmox API token secret"
-  value       = aws_secretsmanager_secret.proxmox_api_token.name
-}
