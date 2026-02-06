f765e81b6fd81c40cf5747a18e688c101f41669a
Sabry <mohamedsabry.dev@gmail.com>
2026-02-06 22:07:13 +0200

    update cross account roles

--- terraform/prod/aws/secrets/main.tf
+++ terraform/prod/aws/secrets/main.tf
@@ -20,3 +20,27 @@ resource "aws_secretsmanager_secret_version" "proxmox_terraform" {
     ignore_changes = [secret_string]
   }
 }
+
+# Read-only token for terraform plan (spans all environments)
+resource "aws_secretsmanager_secret" "proxmox_plan_readonly" {
+  name        = "prod/proxmox/plan-readonly-token"
+  description = "Proxmox read-only API token for Terraform plan (plan_cross_tf@pve)"
+
+  tags = {
+    Environment = "prod"
+    ManagedBy   = "terraform"
+    Purpose     = "proxmox-plan-readonly"
+  }
+}
+
+resource "aws_secretsmanager_secret_version" "proxmox_plan_readonly" {
+  secret_id = aws_secretsmanager_secret.proxmox_plan_readonly.id
+  secret_string = jsonencode({
+    token_id     = "plan_cross_tf@pve!terraform"
+    token_secret = "REPLACE_ME"
+  })
+
+  lifecycle {
+    ignore_changes = [secret_string]
+  }
+}
