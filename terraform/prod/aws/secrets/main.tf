9e24f914fba3b254fd365c80941cb3c2cf1ce5c0
Sabry <mohamedsabry.dev@gmail.com>
2026-02-06 21:13:20 +0200

    add workflow for secret manager production

--- terraform/prod/aws/secrets/main.tf
+++ terraform/prod/aws/secrets/main.tf
@@ -20,13 +20,3 @@ resource "aws_secretsmanager_secret_version" "proxmox_terraform" {
     ignore_changes = [secret_string]
   }
 }
-
-output "secret_arn" {
-  value       = aws_secretsmanager_secret.proxmox_terraform.arn
-  description = "ARN of the Proxmox secret"
-}
-
-output "secret_name" {
-  value       = aws_secretsmanager_secret.proxmox_terraform.name
-  description = "Name of the Proxmox secret"
-}
