c0a5bf6900ae1ea3fe3102b103e86f6e181c8bac
Sabry <mohamedsabry.dev@gmail.com>
2026-02-16 22:39:19 +0200

    update variables

--- terraform/dev/proxmox/vms/golden-image/providers.tf
+++ terraform/dev/proxmox/vms/golden-image/providers.tf
@@ -17,11 +17,11 @@ terraform {
   }
   
   backend "s3" {
-    bucket         = "hybrid-cloud-infrastructure-tf-state-prod"
-    key            = "prod/proxmox/vms/golden-image/terraform.tfstate"
+    bucket         = "hybrid-cloud-infrastructure-tf-state-dev"
+    key            = "dev/proxmox/vms/golden-image/terraform.tfstate"
     region         = "eu-west-2"
     encrypt        = true
-    dynamodb_table = "hybrid-cloud-infrastructure-tf-state-lock-prod"
+    dynamodb_table = "hybrid-cloud-infrastructure-tf-state-lock-dev"
   }
 }
 
@@ -30,7 +30,7 @@ provider "aws" {
   
   default_tags {
     tags = {
-      Environment = "prod"
+      Environment = "dev"
       ManagedBy   = "terraform"
       Module      = "proxmox-golden-image"
     }
