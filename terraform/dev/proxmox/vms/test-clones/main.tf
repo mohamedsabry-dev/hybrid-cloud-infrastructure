c2c1091db7b91d9af8f6b3e5f0b72bd6067754e2
Sabry <mohamedsabry.dev@gmail.com>
2026-02-16 21:20:12 +0200

    test clone vm

--- terraform/dev/proxmox/vms/test-clones/main.tf
+++ terraform/dev/proxmox/vms/test-clones/main.tf
@@ -2,38 +2,6 @@
 # Test VM Clone from Golden Image Template
 #===============================================================================
 
-#-------------------------------------------------------------------------------
-# Cloud-Init Config
-#-------------------------------------------------------------------------------
-resource "proxmox_virtual_environment_file" "cloud_config" {
-  content_type = "snippets"
-  datastore_id = "local"
-  node_name    = var.node_name
-
-  source_raw {
-    data = <<-EOF
-#cloud-config
-hostname: ${var.test_vm.name}
-fqdn: ${var.test_vm.name}.lab.local
-
-chpasswd:
-  list: |
-    root:${data.aws_secretsmanager_secret_version.vm_root.secret_string}
-  expire: false
-
-network:
-  config: disabled
-
-ssh_pwauth: true
-
-runcmd:
-  - systemctl enable qemu-guest-agent
-  - systemctl start qemu-guest-agent
-EOF
-
-    file_name = "cloud-config-${var.test_vm.name}.yaml"
-  }
-}
 
 #-------------------------------------------------------------------------------
 # Clone VM from Template
@@ -69,7 +37,7 @@ resource "proxmox_virtual_environment_vm" "test_vm" {
     dedicated = var.test_vm.memory
   }
 
-  # Cloud-Init configuration
+  ##### Cloud-Init configuration #####
   initialization {
     datastore_id      = var.datastore_id
     user_data_file_id = proxmox_virtual_environment_file.cloud_config.id
