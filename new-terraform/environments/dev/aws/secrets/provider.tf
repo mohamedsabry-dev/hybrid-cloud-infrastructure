9cc6a9e393e31b638d7972d98ed241ca39b939f7
mohamedsabry-dev <mohamedsabry.dev@gmail.com>
2026-02-16 09:24:27 +0200

    test new module secrert

---
+++ new-terraform/environments/dev/aws/secrets/provider.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.28.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "hybrid-cloud-infrastructure-tf-state-dev"
+    key            = "dev/aws/secrets/terraform.tfstate"
+    region         = "eu-west-2"
+    encrypt        = true
+    dynamodb_table = "hybrid-cloud-infrastructure-tf-state-lock-dev"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+locals {
+  tags = {
+    env = "dev"
+  }
+}
