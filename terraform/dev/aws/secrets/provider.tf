3557b8ea789248450b75191a8fc6f8374ba0c8c7
mohamedsabry-dev <mohamedsabry.dev@gmail.com>
2026-02-16 12:31:35 +0200

    update secret workflow

--- terraform/dev/aws/secrets/provider.tf
+++ terraform/dev/aws/secrets/provider.tf
@@ -1,15 +1,13 @@
-
-
 terraform {
   required_version = ">= 1.5.0"
-
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.28.0"
+      version = "6.28.0"  # Pinned for offline runner
     }
   }
-
+  
   backend "s3" {
     bucket         = "hybrid-cloud-infrastructure-tf-state-dev"
     key            = "dev/aws/secrets/terraform.tfstate"
@@ -21,10 +19,12 @@ terraform {
 
 provider "aws" {
   region = "eu-west-2"
-}
-
-locals {
-  tags = {
-    env = "dev"
+  
+  default_tags {
+    tags = {
+      Environment = "dev"
+      ManagedBy   = "terraform"
+      Module      = "secrets"
+    }
   }
 }
