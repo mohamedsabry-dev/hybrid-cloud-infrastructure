138d8d0f9f1edd8c566896f10a4a7ae40982181a
mohamedsabry-dev <mohamedsabry.dev@gmail.com>
2026-02-16 09:24:27 +0200

    test new module secrert

--- new-terraform/environments/dev/aws/secrets/main.tf
+++ new-terraform/environments/dev/aws/secrets/main.tf
@@ -1,8 +1,8 @@
 module "my_lab_secrets" {
-  source = "../../modules/secrets"
+  source = "../../../modules/aws/secrets"
 
   # 1. Proxmox Token
-  proxmox_token_secret_name   = "dev/proxmox/tf-token"
+  proxmox_token_secret_name   = "dev/proxmox/terraform-token"
   proxmox_token_secret_string = jsonencode({
     token_id     = "tf_dev@pve!terraform"
     token_secret = "your-real-token"
@@ -13,7 +13,7 @@ module "my_lab_secrets" {
   }
 
   # 2. SSH Admin
-  proxmox_ssh_admin_secret_name = "dev/proxmox/ssh-admin"
+  proxmox_ssh_admin_secret_name = "dev/proxmox/ssh-admin-password"
   proxmox_ssh_admin_password    = "your-ssh-password"
   proxmox_ssh_admin_secret_tags = {
     Environment = "dev"
@@ -21,7 +21,7 @@ module "my_lab_secrets" {
   }
 
   # 3. VM Root
-  vm_root_password_secret_name = "dev/vm/root-pass"
+  vm_root_password_secret_name = "dev/proxmox/vm-root-password"
   vm_root_password             = "your-root-pass"
   vm_root_password_secret_tags = {
     Environment = "dev"
@@ -29,10 +29,23 @@ module "my_lab_secrets" {
   }
 
   # 4. Gandalf
-  gandalf_password_secret_name = "dev/vm/gandalf-pass"
+  gandalf_password_secret_name = "dev/vm/gandalf-password"
   gandalf_password             = "your-emergency-pass"
   gandalf_password_secret_tags = {
     Environment = "dev"
     Purpose     = "break-glass"
   }
+}
+
+output "proxmox_token_secret_arn" {
+  value = module.my_lab_secrets.proxmox_token_secret_arn
+}
+output "proxmox_ssh_admin_secret_arn" {
+  value = module.my_lab_secrets.proxmox_ssh_admin_secret_arn
+}
+output "vm_root_password_secret_arn" {
+  value = module.my_lab_secrets.vm_root_password_secret_arn
+}
+output "gandalf_password_secret_arn" {
+  value = module.my_lab_secrets.gandalf_password_secret_arn
 }
