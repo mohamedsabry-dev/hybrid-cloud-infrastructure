677c853a7fbf583a8ac21a1655925429855039e2
Sabry <mohamedsabry.dev@gmail.com>
2026-02-21 15:57:20 +0200

    trigger ipa

--- terraform/dev/proxmox/vms/FreeIPA/outputs.tf
+++ terraform/dev/proxmox/vms/FreeIPA/outputs.tf
@@ -3,7 +3,6 @@ output "freeipa" {
   value = {
     vm_id = proxmox_virtual_environment_vm.freeipa.vm_id
     name  = proxmox_virtual_environment_vm.freeipa.name
-    node  = proxmox_virtual_environment_vm.freeipa.node_name
     ip    = var.freeipa.ip
   }
 }
