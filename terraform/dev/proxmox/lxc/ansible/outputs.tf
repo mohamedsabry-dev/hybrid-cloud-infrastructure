61eed4afea51f731a6084f661f00182ded1b53ac
Sabry <mohamedsabry.dev@gmail.com>
2026-02-21 15:56:34 +0200

    trigger ipa

--- terraform/dev/proxmox/lxc/ansible/outputs.tf
+++ terraform/dev/proxmox/lxc/ansible/outputs.tf
@@ -3,7 +3,6 @@ output "ansible" {
   value = {
     container_id = proxmox_virtual_environment_container.ansible.vm_id
     name         = var.ansible.name
-    node         = proxmox_virtual_environment_container.ansible.node_name
     ip           = var.ansible.ip
   }
 }
