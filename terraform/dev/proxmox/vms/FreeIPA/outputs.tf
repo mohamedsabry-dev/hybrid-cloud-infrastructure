c0c2ac94aaffc9985003932e6a8d7d0aa21fe79e
Sabry <mohamedsabry.dev@gmail.com>
2026-02-21 14:07:46 +0200

    test run

--- terraform/dev/proxmox/vms/FreeIPA/outputs.tf
+++ terraform/dev/proxmox/vms/FreeIPA/outputs.tf
@@ -1,9 +1,9 @@
 output "freeipa" {
   description = "freeipa VM information"
   value = {
-    vm_id  = proxmox_virtual_environment_vm.freeipa.vm_id
-    name   = proxmox_virtual_environment_vm.freeipa.name
-    node   = proxmox_virtual_environment_vm.freeipa.node_name
-    ip     = var.freeipa.ip
+    vm_id = proxmox_virtual_environment_vm.freeipa.vm_id
+    name  = proxmox_virtual_environment_vm.freeipa.name
+    node  = proxmox_virtual_environment_vm.freeipa.node_name
+    ip    = var.freeipa.ip
   }
 }
