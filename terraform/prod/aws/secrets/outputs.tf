29c982361f55ba8bfc707cbe4f03f7e6b4151a80
Sabry <mohamedsabry.dev@gmail.com>
2026-02-23 19:22:22 +0200

    update prod secrets

--- terraform/prod/aws/secrets/outputs.tf
+++ terraform/prod/aws/secrets/outputs.tf
@@ -3,12 +3,12 @@
 output "secret_arns" {
   description = "ARNs of created secrets"
   value = {
-    proxmox_api             = aws_secretsmanager_secret.proxmox_terraform.arn
-    proxmox_ssh_admin       = aws_secretsmanager_secret.proxmox_ssh_admin.arn
-    vm_root_password        = aws_secretsmanager_secret.vm_root_password.arn
-    break_glass_password    = aws_secretsmanager_secret.gandalf_password.arn
-    lxc_root_password       = aws_secretsmanager_secret.lxc_root_password.arn
-    ansible_ssh_pubkey      = aws_secretsmanager_secret.ansible_ssh_public_key.arn
+    proxmox_api            = aws_secretsmanager_secret.proxmox_terraform.arn
+    proxmox_ssh_admin      = aws_secretsmanager_secret.proxmox_ssh_admin.arn
+    vm_root_password       = aws_secretsmanager_secret.vm_root_password.arn
+    break_glass_password   = aws_secretsmanager_secret.break_glass_password.arn
+    lxc_root_password      = aws_secretsmanager_secret.lxc_root_password.arn
+    ansible_ssh_pubkey     = aws_secretsmanager_secret.ansible_ssh_pubkey.arn
     local_runner_ssh_pubkey = aws_secretsmanager_secret.local_runner_ssh_pubkey.arn
   }
 }
@@ -16,12 +16,12 @@ output "secret_arns" {
 output "secret_names" {
   description = "Names of created secrets for CLI reference"
   value = {
-    proxmox_api             = aws_secretsmanager_secret.proxmox_terraform.name
-    proxmox_ssh_admin       = aws_secretsmanager_secret.proxmox_ssh_admin.name
-    vm_root_password        = aws_secretsmanager_secret.vm_root_password.name
-    break_glass_password    = aws_secretsmanager_secret.gandalf_password.name
-    lxc_root_password       = aws_secretsmanager_secret.lxc_root_password.name
-    ansible_ssh_pubkey      = aws_secretsmanager_secret.ansible_ssh_public_key.name
+    proxmox_api            = aws_secretsmanager_secret.proxmox_terraform.name
+    proxmox_ssh_admin      = aws_secretsmanager_secret.proxmox_ssh_admin.name
+    vm_root_password       = aws_secretsmanager_secret.vm_root_password.name
+    break_glass_password   = aws_secretsmanager_secret.break_glass_password.name
+    lxc_root_password      = aws_secretsmanager_secret.lxc_root_password.name
+    ansible_ssh_pubkey     = aws_secretsmanager_secret.ansible_ssh_pubkey.name
     local_runner_ssh_pubkey = aws_secretsmanager_secret.local_runner_ssh_pubkey.name
   }
 }
