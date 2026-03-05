859bba1fe974b5cb813443f8baf779023683325a
Sabry <mohamedsabry.dev@gmail.com>
2026-03-05 13:58:46 +0200

    Setup Ansible

--- terraform/dev/proxmox/lxc/ansible/main.tf
+++ terraform/dev/proxmox/lxc/ansible/main.tf
@@ -10,11 +10,11 @@ resource "proxmox_virtual_environment_container" "ansible" {
   vm_id     = var.ansible.ctid
   tags      = ["lxc", "ansible", "infrastructure"]
 
-  # Privileged container (required for FreeIPA high UIDs)
-  unprivileged = false
+  # Unprivileged container with nesting
+  unprivileged = true
 
   features {
-    nesting = false
+    nesting = true
   }
 
   # Operating System Template
