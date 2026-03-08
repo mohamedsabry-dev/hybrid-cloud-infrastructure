2a9809c086a533dc8e5b03f41580f2cfcf2edfd9
Sabry <mohamedsabry.dev@gmail.com>
2026-03-08 19:33:12 +0200

    Setup Backup Config and Snapshot Fix to PROD

---
+++ terraform/prod/proxmox/storage/nas/outputs.tf
@@ -0,0 +1,9 @@
+output "nas_iso_id" {
+  description = "The ID of the NAS ISO storage"
+  value       = proxmox_virtual_environment_storage_nfs.nas_iso.id
+}
+
+output "nas_data_id" {
+  description = "The ID of the NAS data storage"
+  value       = proxmox_virtual_environment_storage_nfs.nas_data.id
+}
