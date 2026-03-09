6c22f46a84e5db5353b1739895044f7b4f2627c8
mohamedsabry-dev <mohamedsabry.dev@gmail.com>
2026-03-09 12:40:04 +0200

    Draft AWS Wireguard tf file

---
+++ terraform/dev/aws/network/main.tf
@@ -0,0 +1,58 @@
+resource "aws_vpc" "vpc_main" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "dev-main"
+  }
+}
+
+resource "aws_internet_gateway" "igw_main" {
+  vpc_id = aws_vpc.vpc_main.id
+
+  tags = {
+    Name = "dev-igw"
+  }
+}
+
+resource "aws_subnet" "subnet_vpn" {
+  vpc_id            = aws_vpc.vpc_main.id
+  cidr_block        = "172.16.65.0/24"
+  availability_zone = "eu-west-2a"
+
+  tags = {
+    Name = "dev-vpn"
+  }
+}
+
+resource "aws_subnet" "subnet_mgmt" {
+  vpc_id            = aws_vpc.vpc_main.id
+  cidr_block        = "172.16.63.0/24"
+  availability_zone = "eu-west-2a"
+
+  tags = {
+    Name = "dev-mgmt"
+  }
+}
+
+resource "aws_route_table" "rt_public" {
+  vpc_id = aws_vpc.vpc_main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw_main.id
+  }
+
+  tags = {
+    Name = "dev-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "rta_vpn" {
+  subnet_id      = aws_subnet.subnet_vpn.id
+  route_table_id = aws_route_table.rt_public.id
+}
+
+resource "aws_route_table_association" "rta_mgmt" {
+  subnet_id      = aws_subnet.subnet_mgmt.id
+  route_table_id = aws_route_table.rt_public.id
+}
---
+++ terraform/dev/aws/network/provider.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = ">= 1.5.0"
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.28.0"  # Pinned for offline runner
+    }
+  }
+  
+  backend "s3" {
+    bucket         = "hybrid-cloud-infrastructure-tf-state-dev"
+    key            = "dev/aws/network/terraform.tfstate"
+    region         = "eu-west-2"
+    encrypt        = true
+    dynamodb_table = "hybrid-cloud-infrastructure-tf-state-lock-dev"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+  
+  default_tags {
+    tags = {
+      Environment = "dev"
+      ManagedBy   = "terraform"
+      Module      = "network"
+    }
+  }
+}
