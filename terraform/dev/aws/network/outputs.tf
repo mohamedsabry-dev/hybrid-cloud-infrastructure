ced8183830d020f554d263adc8fcc2c320e07e12
mohamedsabry-dev <mohamedsabry.dev@gmail.com>
2026-03-09 12:40:04 +0200

    Draft AWS Wireguard tf file

---
+++ terraform/dev/aws/network/outputs.tf
@@ -0,0 +1,19 @@
+output "rt_public_id" {
+  value = aws_route_table.rt_public.id
+}
+
+output "rt_public_id" {
+  value = aws_route_table.rt_public.id
+}
+
+output "vpc_id" {
+  value = aws_vpc.vpc_main.id
+}
+
+output "subnet_vpn_id" {
+  value = aws_subnet.subnet_vpn.id
+}
+
+output "subnet_mgmt_id" {
+  value = aws_subnet.subnet_mgmt.id
+}
