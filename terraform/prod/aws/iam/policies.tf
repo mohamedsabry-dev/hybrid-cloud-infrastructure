d64a852b91a506e733e47187a90600db678d0084
Sabry <mohamedsabry.dev@gmail.com>
2026-03-11 19:23:24 +0200

    Setup Allow Role

--- terraform/prod/aws/iam/policies.tf
+++ terraform/prod/aws/iam/policies.tf
@@ -61,21 +61,12 @@ resource "aws_iam_policy" "security_boundary_prod" {
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
-      # Block IAM mutation (except PassRole)
+      # Block IAM mutation
       {
-        Sid       = "DenyIAMMutation"
-        Effect    = "Deny"
-        NotAction = ["iam:PassRole", "iam:GetInstanceProfile"]
-        Resource  = "*"
-      },
-      # Allow PassRole only for specific EC2 roles
-      {
-        Sid      = "DenyPassRoleExceptAllowed"
+        Sid      = "DenyIAMMutation"
         Effect   = "Deny"
-        Action   = "iam:PassRole"
-        NotResource = [
-          "arn:aws:iam::${var.prod_account_id}:role/prod-wireguard-ssm-role"
-        ]
+        Action   = "iam:*"
+        Resource = "*"
       },
       # Block CloudTrail
       {
