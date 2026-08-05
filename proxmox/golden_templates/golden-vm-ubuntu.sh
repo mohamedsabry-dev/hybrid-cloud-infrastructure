480a8d5245089c6d9b5669874815dfab4312ae36
Mohamed Sabry <mohamedsabry.dev@gmail.com>
2026-08-05 21:53:43 +0300

    template draft for script

---
+++ proxmox/golden_templates/golden-vm-ubuntu.sh
@@ -0,0 +1,40 @@
+## Layer 1 Setup binary, debug and safety mesure 
+
+#!/bin/bash
+set -euo pipefail
+
+# Forward the full run details + Error if any , stdin,stdout,err to debug_script name 
+
+
+# Layer 2 Variables 
+Collection_Network=(net-tools traceroute wget)
+Collection_Virtu=(agent)
+Collection_Tools=(vim htop)
+
+
+
+
+# Layer 3 Interaction notice about script actions 
+
+# We gonna do x,y,z
+
+# Layer 4 Function of excution 
+# 1.0 Update && upgrade 
+# 1.1 Install Network Package
+# 1.2 Vitualization needs 
+# 1.3 Tools 
+# 1.4 somthning else if needed 
+
+# Layer 5 function of confirmation 
+
+# Ensure package installed 
+
+
+# Layer 6 Interaction notice abotu what done 
+# x,y,z done , confirmation evidence .. Call function confirmation on all colelctions with for or manual 3 calls but better with for 
+
+# 2.0 Clean Up Function [Host info and fingreprint]
+
+# Layer 7 Customized basic config 
+
+# Layer 8 Service enable 
